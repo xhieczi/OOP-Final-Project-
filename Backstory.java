dc39ceefd12b0fa714e30eabc4ccb81c34767be2
k-nj <dirkkenjibalili27@gmail.com>
2025-10-06 01:14:14 +0800

    Update Backstory.java
    - Made into abstraction

--- Backstory.java
+++ Backstory.java
@@ -1,3 +1,9 @@
-public class Backstory{
-   
+import java.io.*;
+
+abstract class Backstory {
+    public static PrintStream prt = System.out;
+    public abstract void showBackstoryMain(); // Based from the original characters of "Encantadia"
+    public abstract void showBackstoryOG(); // Original Characters
 }
+
+
