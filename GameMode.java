86fac10341c224488c0ddbef9599e8f82497aef8
xhieczi <jelianlimetares@gmail.com>
2025-10-09 17:50:31 +0800

    Create GameMode.java

---
+++ GameMode.java
@@ -0,0 +1,38 @@
+package FinalProject;
+
+import java.io.PrintStream;
+import java.util.*;
+
+public class GameMode {
+    static Scanner sc = new Scanner(System.in);
+    static PrintStream prt = System.out;
+
+    static int chooseGameMode() {
+        prt.print("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
+        for (int i = 1; i <= 15; i++) prt.print("=====");
+        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+ ✦✦✦ Choose Your Game Mode ✦✦✦ +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+ [1] Player vs Player (Manual Battle)  +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+ [2] Player vs Enemy (Normal Mode)     +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+ [3] Arcade Mode (Player vs All)       +", 8);
+        prt.print("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
+        for (int i = 1; i <= 15; i++) prt.print("=====");
+        EncantadiaGame.typePrintInline("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tEnter your choice: ", 3);
+
+        int mode = sc.nextInt();
+        sc.nextLine(); // clear buffer
+
+        String modeName = switch (mode) {
+            case 1 -> "Player vs Player (Manual Battle)";
+            case 2 -> "Player vs Enemy (Normal Mode)";
+            case 3 -> "Arcade Mode (Player vs All)";
+            default -> "Invalid Mode";
+        };
+
+        prt.print("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
+        for (int i = 1; i <= 15; i++) prt.print("=====");
+        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+ [🎮] You chose " + modeName + "! +", 8);
+        prt.print("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
+        for (int i = 1; i <= 15; i++) prt.print("=====");
+        return mode;
+    }
+}
