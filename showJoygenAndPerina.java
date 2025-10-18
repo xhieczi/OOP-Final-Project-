d7309d604149748cf601e2fbe8b34d01d8fe9e2f
k-nj <dirkkenjibalili27@gmail.com>
2025-10-18 16:19:52 +0800

    Update showJoygenAndPerina.java

--- showJoygenAndPerina.java
+++ showJoygenAndPerina.java
@@ -1,19 +1,19 @@
 public class showJoygenAndPerina extends Backstory {
     @Override
     public void showBackstoryMain(){
-        prt.print("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
-        for (int i2 = 1; i2 <= 17; i2++) {
+        prt.print("\t\t\t\t\t\t");
+        for (int i2 = 1; i2 <= 32; i2++) {
             prt.print("=====");
         }
 
-        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t +", 8);
-        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+ Born from a spark that lit the first star, controls fire to create life or destroy evil.  +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+ She can purify souls with flames, command sunlight to grow crops or burn lands, forge weapons that never break, and help beings rise a new from ashes. +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+ But her power fades in darkness, and if misused, her fires rage uncontrollably. +", 8);
-        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t +", 8);
-        prt.println();
-        prt.print("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
-        for (int i2 = 1; i2 <= 17; i2++) {
+        EncantadiaGame.typePrint("\n\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[Joygen's Backstory]\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\tBorn from a spark that lit the first star, controls fire to create life or destroy evil.\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t  She can purify souls with flames, command sunlight to grow crops or burn lands, forge weapons that never break, and help beings rise a new from ashes.   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\tBut her power fades in darkness, and if misused, her fires rage uncontrollably.\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        prt.print("\t\t\t\t\t\t");
+        for (int i2 = 1; i2 <= 32; i2++) {
             prt.print("=====");
         }
         prt.println();
@@ -21,18 +21,20 @@ public class showJoygenAndPerina extends Backstory {
 
     @Override
     public void showBackstoryOG() {
-        prt.print("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
-        for (int i2 = 1; i2 <= 17; i2++) {
+        prt.println();
+        prt.println();
+        prt.print("\t");
+        for (int i2 = 1; i2 <= 40; i2++) {
             prt.print("=====");
         }
-        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t +", 8);
-        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+ Born when the moon’s shattered core bled into the mortal realm, carries a soul bound to fractured starlight a curse that grants her power to mend broken realms but drains mortal hearts of hope. +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+ She commands flames to incinerate enemies, ignite weapons with scorching power. +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+ However, her power diminishes in the presence of strong water magic or when faced with overwhelming grief, as her strength is fueled by passion and will.\n +", 8);
-        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t +", 8);
-        prt.println();
-        prt.print("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
-        for (int i2 = 1; i2 <= 17; i2++) {
+        EncantadiaGame.typePrint("\n\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[Perina's Backstory]\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t+   Born when the moon’s shattered core bled into the mortal realm, carries a soul bound to fractured starlight a curse that grants her power to mend broken realms but drains mortal hearts of hope.  +", 8);
+        EncantadiaGame.typePrint("\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tShe commands flames to incinerate enemies, ignite weapons with scorching power.\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t+\t\t\t\t\t\t\tHowever, her power diminishes in the presence of strong water magic or when faced with overwhelming grief, as her strength is fueled by passion and will.\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        prt.print("\t");
+        for (int i2 = 1; i2 <= 40; i2++) {
             prt.print("=====");
         }
         prt.println();
