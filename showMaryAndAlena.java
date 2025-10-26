e292ea33bbb1efc903c292a295392bf4684607bf
k-nj <dirkkenjibalili27@gmail.com>
2025-10-26 18:42:53 +0800

    Update showMaryAndAlena.java

--- showMaryAndAlena.java
+++ showMaryAndAlena.java
@@ -1,45 +1,48 @@
 public class showMaryAndAlena extends Backstory {
     @Override
     public void showBackstoryMain(){
-        prt.print("\n\t\t\t\t\t\t\t\t\t");
-        for (int i2 = 1; i2 <= 17; i2++) {
+        prt.println();
+        prt.println();
+        prt.print("\t\t\t\t");
+        for (int i2 = 1; i2 <= 32; i2++) {
             prt.print("=====");
         }
-        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t\t+   ✦✦✦✦✦✦✦✦✦✦✦               [ Sangre Mary ]                ✦✦✦✦✦✦✦✦✦✦✦    +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+                                                                                   +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+   Born from the first meeting of moonlight and ocean, a child                     +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+   of rhythm and eternal pull. Her spirit carries the dual                        +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+   ature of the sea—gentle enough to cradle life, yet fierce enough               +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+    to sweep entire worlds away.                                                  +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+                                                                                   +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+   ✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦   +", 8);
-        prt.print("\t\t\t\t\t\t\t\t\t");
-        for (int i2 = 1; i2 <= 17; i2++) {
+
+        EncantadiaGame.typePrint("\n\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[Mary's Backstory]\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\t\t\tBorn from the first meeting of moonlight and ocean, a child of rhythm and eternal pull.\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\tHer spirit carries the dual nature of the sea—gentle enough to cradle life, yet fierce enough to sweep entire worlds away.\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\t\t\t\tShe thrives in twilight, where hidden truths slip through the veil of shadows.\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+
+        prt.print("\t\t\t\t");
+        for (int i2 = 1; i2 <= 32; i2++) {
             prt.print("=====");
         }
+        prt.println();
         prt.println();
     }
 
     @Override
     public void showBackstoryOG() {
-        prt.print("\n\n\t\t\t\t\t\t\t\t\t");
-        for (int i2 = 1; i2 <= 17; i2++) {
+        prt.println();
+        prt.println();
+        prt.print("\t\t\t\t");
+        for (int i2 = 1; i2 <= 32; i2++) {
             prt.print("=====");
         }
+        EncantadiaGame.typePrint("\n\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[Alena's Backstory]\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\tBorn with a voice as fluid and powerful as the rivers, a gift that could heal wounds and calm raging storms.\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\tBeloved for her kindness yet tested by envy and betrayal, she learned that even the purest waters must carve their own path.\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\tHer presence is the promise of change, ever restless and ever watchful.\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
 
-        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t\t+   ✦✦✦✦✦✦✦✦✦✦✦                  [ Danaya ]                   ✦✦✦✦✦✦✦✦✦✦✦    +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+                                                                                     +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+   Born with a voice as fluid and powerful as the rivers,                            +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+   a gift that could heal wounds and calm raging storms.                             +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+   Beloved for her kindness yet tested by envy and betrayal,                         +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+   she learned that even the purest waters must carve their own path.             +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+                                                                                      +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+   ✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦   +", 8);
-
-        prt.print("\t\t\t\t\t\t\t\t\t");
-        for (int i2 = 1; i2 <= 17; i2++) {
+        prt.print("\t\t\t\t");
+        for (int i2 = 1; i2 <= 32; i2++) {
             prt.print("=====");
         }
+        prt.println();
         prt.println();
     }
 }
