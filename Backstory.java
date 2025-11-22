9aa9065fcd47e22679c16eafa2eec0e03cf26824
k-nj <dirkkenjibalili27@gmail.com>
2025-11-22 16:14:41 +0800

    Update Backstory.java
    updated

--- Backstory.java
+++ Backstory.java
@@ -1,9 +1,5 @@
-import java.io.*;
 
-abstract class Backstory {
-    public static PrintStream prt = System.out;
+public abstract class Backstory {
     public abstract void showBackstoryMain(); // Based from the original characters of "Encantadia"
     public abstract void showBackstoryOG(); // Original Characters
 }
-
-
