2cd4bc977ab10db50f79078926dbbc24439511a1
xhieczi <jelianlimetares@gmail.com>
2025-11-22 16:43:06 +0800

    Update Backstory.java

--- Backstory.java
+++ Backstory.java
@@ -1,5 +1,8 @@
 
-public abstract class Backstory {
+import java.io.*;
+
+abstract class Backstory {
+    public static PrintStream prt = System.out;
     public abstract void showBackstoryMain(); // Based from the original characters of "Encantadia"
     public abstract void showBackstoryOG(); // Original Characters
 }
