00100d4300ef042ff20ee6b4a0a0338b1ca9ba2f
Mary Claire <maryclaire.lauron@cit.edu>
2025-11-24 15:54:59 +0800

    Updated_Character

--- Character.java
+++ Character.java
@@ -1,40 +1,52 @@
-import java.util.Scanner;
 import java.util.Random;
-import java.io.*;
 
 public class Character {
     String name;
-    String backstory;
+    String element;
     String[] skills;
     int[] manaCost;
-    int[] damage;
-    int hp = 500;
+    int[][] damageRange; // [min, max] for each skill
+    int health;
 
+    private Random rand = new Random();
 
-    Character(String name, String backstory, String[] skills, int[] manaCost, int[] damage) {
+    // Constructor
+    Character(String name, String element, String[] skills, int[] manaCost, int[][] damageRange) {
         this.name = name;
-        this.backstory = backstory;
+        this.element = element;
         this.skills = skills;
         this.manaCost = manaCost;
-        this.damage = damage;
+        this.damageRange = damageRange;
+        this.health = 500; // default health
     }
 
+    // Check if character is alive
+    boolean isAlive() {
+        return health > 0;
+    }
+
+    // Get random damage for a skill
+    public int getRandomDamage(int skillIndex) {
+        int min = damageRange[skillIndex][0];
+        int max = damageRange[skillIndex][1];
+        return rand.nextInt(max - min + 1) + min;
+    }
+
+    // Display skills
     void displaySkills() {
-        System.out.println("Choose a skill:");
+        System.out.println(name + " - Choose a skill:");
         for (int i = 0; i < skills.length; i++) {
             System.out.println((i + 1) + ". " + skills[i] +
                     " (Mana: " + manaCost[i] +
-                    ", Damage: " + damage[i] + ")");
+                    ", Damage: " + damageRange[i][0] + "-" + damageRange[i][1] + ")");
         }
     }
 
+    // Attack another character using a skill
     void attack(Character target, int skillIndex) {
-        int dmg = damage[skillIndex];
-        target.hp -= dmg;
-        if (target.hp < 0) target.hp = 0;
-        System.out.println(name + " used " + skills[skillIndex] +
-                "! Damage: " + dmg);
+        int dmg = getRandomDamage(skillIndex);
+        target.health -= dmg;
+        if (target.health < 0) target.health = 0;
+        System.out.println(name + " used " + skills[skillIndex] + "! Damage: " + dmg);
     }
-
 }
-
