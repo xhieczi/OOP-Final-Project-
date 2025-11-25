9898c1bcb808a1882ba536a8f5dc84cfff174ed9
k-nj <dirkkenjibalili27@gmail.com>
2025-11-25 11:56:34 +0800

    Update showMaryAndAlena.java

--- showMaryAndAlena.java
+++ showMaryAndAlena.java
@@ -8,11 +8,11 @@ public class showMaryAndAlena extends Backstory {
             EncantadiaGame.prt.print("=====");
         }
 
-        EncantadiaGame.typePrint("\n\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[Mary's Backstory]\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\n\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[Mary's Backstory]\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
         EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\tBorn from the first meeting of moonlight and ocean, a child of rhythm and eternal pull.\t\t\t\t\t\t\t\t\t   +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\tHer spirit carries the dual nature of the sea—gentle enough to cradle life, yet fierce enough to sweep entire worlds away.\t\t\t   +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\tShe thrives in twilight, where hidden truths slip through the veil of shadows.\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\tBorn from the first meeting of moonlight and ocean, a child of rhythm and eternal pull.\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\tHer spirit carries the dual nature of the sea—gentle enough to cradle life, yet fierce enough to sweep entire worlds away.\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\tShe thrives in twilight, where hidden truths slip through the veil of shadows.\t\t\t\t\t\t\t\t\t\t\t   +", 8);
         EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
 
         EncantadiaGame.prt.print("\t\t\t\t\t\t");
@@ -31,12 +31,12 @@ public class showMaryAndAlena extends Backstory {
         for (int i2 = 1; i2 <= 32; i2++) {
             EncantadiaGame.prt.print("=====");
         }
-        EncantadiaGame.typePrint("\n\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[Alena's Backstory]\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\tBorn with a voice as fluid and powerful as the rivers, a gift that could heal wounds and calm raging storms.\t\t\t\t\t   +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\tBeloved for her kindness yet tested by envy and betrayal, she learned that even the purest waters must carve their own path.\t\t   +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\tHer presence is the promise of change, ever restless and ever watchful.\t\t\t\t\t\t\t\t\t\t\t   +", 8);
-        EncantadiaGame.typePrint("\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\n\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[Alena's Backstory]\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\tBorn with a voice as fluid and powerful as the rivers, a gift that could heal wounds and calm raging storms.\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\tBeloved for her kindness yet tested by envy and betrayal, she learned that even the purest waters must carve their own path.\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\tHer presence is the promise of change, ever restless and ever watchful.\t\t\t\t\t\t\t\t\t\t\t   +", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   +", 8);
 
         EncantadiaGame.prt.print("\t\t\t\t");
         for (int i2 = 1; i2 <= 32; i2++) {
