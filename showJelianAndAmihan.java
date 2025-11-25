4f5e4f3755de53949ff01ca0ca0028d3c37c7511
k-nj <dirkkenjibalili27@gmail.com>
2025-11-25 11:56:58 +0800

    Update showJelianAndAmihan.java

--- showJelianAndAmihan.java
+++ showJelianAndAmihan.java
@@ -5,18 +5,18 @@ public class showJelianAndAmihan extends Backstory {
     public void showBackstoryMain(){
         EncantadiaGame.prt.println();
         EncantadiaGame.prt.println();
-        EncantadiaGame.prt.print("\t\t\t\t\t\t\t\t");
+        EncantadiaGame.prt.print("\t\t\t\t\t\t\t\t\t");
         for (int i2 = 1; i2 <= 29; i2++) {
             EncantadiaGame.prt.print("=====");
         }
 
-        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[Jelian's Backstory]\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t+ \t\tBorn from the silence between heartbeats, Jelian emerged as the keeper of secrets that mortals and gods alike feared to speak aloud.\t+", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t+ \t\t\t\tHer voice is never heard directly but carried through echoes and murmurs, guiding those who listen with patience.\t\t\t\t+", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t+ \t\t\t\t\t\t\t\t\tShe thrives in twilight, where hidden truths slip through the veil of shadows.\t\t\t\t\t\t\t\t+", 8);
-        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+", 8);
-        EncantadiaGame.prt.print("\t\t\t\t\t\t\t\t");
+        EncantadiaGame.typePrint("\n\t\t\t\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[Jelian's Backstory]\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+ \t\tBorn from the silence between heartbeats, Jelian emerged as the keeper of secrets that mortals and gods alike feared to speak aloud.\t+", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+ \t\t\t\tHer voice is never heard directly but carried through echoes and murmurs, guiding those who listen with patience.\t\t\t\t+", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+ \t\t\t\t\t\t\t\t\tShe thrives in twilight, where hidden truths slip through the veil of shadows.\t\t\t\t\t\t\t\t+", 8);
+        EncantadiaGame.typePrint("\t\t\t\t\t\t\t\t\t+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t+", 8);
+        EncantadiaGame.prt.print("\t\t\t\t\t\t\t\t\t");
         for (int i2 = 1; i2 <= 29; i2++) {
             EncantadiaGame.prt.print("=====");
         }
