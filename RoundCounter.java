7302947b45e59e4d1f1c08f8b9858610c557dd1a
k-nj <dirkkenjibalili27@gmail.com>
2025-11-28 00:13:56 +0800

    Update RoundCounter.java
    Goods na mo ihap

--- RoundCounter.java
+++ RoundCounter.java
@@ -1,8 +1,11 @@
+import java.io.PrintStream;
+
 public class RoundCounter {
     private int round;
     private int player1Wins;
     private int player2Wins;
 
+    // Assuming printingAsciiArts is in the same package
     public printingAsciiArts pr = new printingAsciiArts();
 
     public RoundCounter() {
@@ -15,14 +18,6 @@ public class RoundCounter {
         return round;
     }
 
-    public int getPlayer1Wins() {
-        return player1Wins;
-    }
-
-    public int getPlayer2Wins() {
-        return player2Wins;
-    }
-
     public void player1WinsRound() {
         player1Wins++;
         round++;
@@ -33,26 +28,21 @@ public class RoundCounter {
         round++;
     }
 
-    public void reset() {
-        round = 1;
-        player1Wins = 0;
-        player2Wins = 0;
-    }
-
-    public void displayRoundScores(EncantadiaGame.Character player1, EncantadiaGame.Character player2) {
-        System.out.println();
-        pr.tabPrinter(20);
-        EncantadiaGame.typePrint("========================================================",8);
-        pr.tabPrinter(20);
-        System.out.println("        ROUND " + round);
-        pr.tabPrinter(4);
-        System.out.println("Score: " + player1.name + " [" + player1Wins + "] - " + player2.name + " [" + player2Wins + "]");
-        pr.tabPrinter(20);
-        EncantadiaGame.typePrint("=========================================================",8);
-    }
-
     public boolean isMatchOver() {
+        // Best of 3 means the first to reach 2 wins
         return player1Wins >= 2 || player2Wins >= 2;
+    }
+
+    // Displays Round AND Current Score
+    public void displayRoundScores(EncantadiaGame.Character player1, EncantadiaGame.Character player2) {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        pr.tabPrinter(19);
+        EncantadiaGame.typePrint("   ========================================================", 8);
+        pr.tabPrinter(23);
+        // Added Score Display here:
+        System.out.println("           ROUND " + round + "  |  SCORE: [" + player1Wins + " - " + player2Wins + "]");
+        pr.tabPrinter(19);
+        EncantadiaGame.typePrint("   =========================================================", 8);
     }
 
     public EncantadiaGame.Character getMatchWinner(EncantadiaGame.Character player1, EncantadiaGame.Character player2) {
