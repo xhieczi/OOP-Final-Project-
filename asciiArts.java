cf787faa212ce2b9da50cf6eb87a9c7e933af315
k-nj <dirkkenjibalili27@gmail.com>
2025-12-05 11:55:26 +0800

    Update asciiArts.java

--- asciiArts.java
+++ asciiArts.java
@@ -1,5 +1,10 @@
 interface asciiArts {
     void tabPrinter(int amount);
     void printLoadingScreen(int offset);
+    void printBackgroundScene();
+    void printPVP();
+    void printPVE();
+    void printARCADE();
+    void lostScreen();
     //void printBootScreen();
 }
